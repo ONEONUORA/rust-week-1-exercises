9791583fb2fa8e47b08515a880e453e654b0c716
ONEONUORA <monweebube44@gmail.com>
2025-06-20 16:57:49 -0700

    feat: add function implemeentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,15 @@
 // Implement extract_tx_version function below
 pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
-    todo!()
+
+    let tx_bytes = hex::decode(raw_tx_hex).map_err(|_| "Hex decode error".to_string())?;
+
+    
+    if tx_bytes.len() < 4 {
+        return Err("Transaction data too short".to_string());
+    }
+
+    let version_bytes: [u8; 4] = tx_bytes[0..4].try_into().unwrap();
+    let version = u32::from_le_bytes(version_bytes);
+
+    Ok(version)
 }
